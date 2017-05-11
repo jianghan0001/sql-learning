fddf8be67d7bea839e51fe21f286435a98b23e7f
jianghan0001 <jianghan0001@gmail.com>
2017-05-11 00:10:49 -0700

    11

--- ETL/ETL note/module 2 - transformation.sql
+++ ETL/ETL note/module 2 - transformation.sql
@@ -8,3 +8,65 @@
 -- case when then else end, iif(t3.[size] is null, 'one size', t3.[size]) (条件，是的话，不是的话)
 -- isnull(where, change to) 
  
+declare @startDate date
+declare @endDate date
+select @startDate = '01-01-' + convert(nvarchar(100), year(min(orderdate)))
+	from salesorderheader
+select @endDate = '12-31-'+ convert(nvarchar(100), year(max(orderdate)))
+	from salesOrderHeader
+
+
+declare @dateInprocess date = @startDate
+
+while @dateInprocess <= @endDate
+	begin
+	insert into dimDate
+	values(
+		convert(nvarchar(100) , @dateInprocess, 112) --[calendarDateKey],
+		dateName(weekday, @dateInprocess) + ', ' + convert(nvarchar(50), @dateInprocess),
+		set @dateInprocess = dateadd(d, 1 , @dateInprocess)
+	end
+
+create procedure pETLProcedureTemplate
+as
+begin
+declare @RC int  = 0
+	begin try
+		begin transaction
+		select 3/0
+		commit transaction
+		set @RC = 100
+	end try
+	begin catch
+	rollback tran
+	set @RC = -100
+	end catch
+	return @RC
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
